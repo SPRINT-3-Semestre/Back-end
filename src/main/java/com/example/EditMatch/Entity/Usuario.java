63b6b1f0ebe3040421bd8f66e8aa5ef03095eda0
VictorXdAugusto <victor.pereira@sptech.school>
2023-10-07 03:54:44 -0300

    adding some validations to user fields

--- src/main/java/com/example/EditMatch/Entity/Usuario.java
+++ src/main/java/com/example/EditMatch/Entity/Usuario.java
@@ -4,8 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -16,20 +15,42 @@ abstract public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    @NotBlank
+
+    @NotBlank(message = "O nome não pode estar em branco")
     private String nome;
+
     private String last_name;
+
+    @Pattern(regexp = "\\d{9}", message = "RG deve conter 9 dígitos numéricos")
     private String rg;
+
+    @Pattern(regexp = "\\d{11}", message = "CPF deve conter 11 dígitos numéricos")
     private String cpf;
+
+    @Past(message = "A data de nascimento deve estar no passado")
     private LocalDate birth;
+
+    @Min(value = 0, message = "O valor mínimo para gender é 0")
+    @Max(value = 1, message = "O valor máximo para gender é 1")
     private Integer gender;
+
     private Boolean is_editor;
+
+    @Email(message = "O formato do e-mail é inválido")
     private String email;
+
+    @Size(min = 8, message = "A senha deve ter pelo menos 8 caracteres")
     private String password;
+
     private String photo_profile;
+
     private String desc_profile;
+
     private LocalDateTime created_at;
+
     private LocalDateTime updated_at;
+
+    @Future(message = "A data de entrega deve estar no futuro")
     private LocalDate dataEntrega;
 
     public Usuario() {
