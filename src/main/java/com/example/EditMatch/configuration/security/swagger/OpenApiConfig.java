3306f4fc2b6976b289b03273d87255777117f147
VictorXdAugusto <victor.pereira@sptech.school>
2023-10-25 10:02:51 -0300

    fixing swagger settings

--- src/main/java/com/example/EditMatch/configuration/security/swagger/OpenApiConfig.java
+++ src/main/java/com/example/EditMatch/configuration/security/swagger/OpenApiConfig.java
@@ -2,21 +2,31 @@ package com.example.EditMatch.configuration.security.swagger;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(
-        info = @Info(
-                title = "Projeto EditMatch",
-                description = "Projeto do 3º semestre da faculdade Sptech",
-                version = "1.0.0"
-        )
-)
-@SecurityScheme(
-        name = "Bearer", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT"
-)
 public class OpenApiConfig {
+        @Bean
+        public OpenAPI customOpenAPI() {
+                return new OpenAPI()
+                        .components(new Components()
+                                .addSecuritySchemes("bearer-key",
+                                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                        .info(new Info()
+                                .title("Connecti Api")
+                                .description("")
+                                .contact(new Contact()
+                                        .email("Victor.pereira@sptech.school")))
+                        .addSecurityItem(new SecurityRequirement().addList("bearer-key"));
+        }
 
 }
+
